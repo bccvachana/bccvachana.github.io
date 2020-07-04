cc34302217f10c3b6b935def3ec69ba5289c3f90
Vachana Chongruchipinyo <bccvachana@gmail.com>
2020-07-05 03:51:16 +0700

    Updates

---
+++ precache-manifest.94146cfaadece9a62bc12eafb75c3486.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697ba10626a6440c490950275cfdca1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76dc0d10b3187d94b551",
+    "url": "/static/css/main.361eb00f.chunk.css"
+  },
+  {
+    "revision": "e4e0b6a884f3757aa7fc",
+    "url": "/static/js/2.42800ffa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42800ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76dc0d10b3187d94b551",
+    "url": "/static/js/main.de8c9056.chunk.js"
+  },
+  {
+    "revision": "9b1312763ecc800e9ffd",
+    "url": "/static/js/runtime-main.4cbfffd7.js"
+  },
+  {
+    "revision": "e62b3130a1faa3434353e3c7f5ff2664",
+    "url": "/static/media/Prompt-Black.e62b3130.ttf"
+  },
+  {
+    "revision": "befa1f27b3fbec48e74d58e8f7d61bb8",
+    "url": "/static/media/Prompt-Bold.befa1f27.ttf"
+  },
+  {
+    "revision": "45f79fcebe7cf35186daed3a2e4e3260",
+    "url": "/static/media/Prompt-ExtraBold.45f79fce.ttf"
+  },
+  {
+    "revision": "cec66179811c4a71396216ef7214f4a0",
+    "url": "/static/media/Prompt-ExtraLight.cec66179.ttf"
+  },
+  {
+    "revision": "2aed5273600290713f7ed8d182a93592",
+    "url": "/static/media/Prompt-Light.2aed5273.ttf"
+  },
+  {
+    "revision": "bd536c6341c5402b41fa4b9e58705579",
+    "url": "/static/media/Prompt-Medium.bd536c63.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/static/media/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "ba9b20ec2ff176605b2d69723af3a54f",
+    "url": "/static/media/Prompt-SemiBold.ba9b20ec.ttf"
+  },
+  {
+    "revision": "ddcfbf895b67206d003167df83c7020a",
+    "url": "/static/media/Prompt-Thin.ddcfbf89.ttf"
+  },
+  {
+    "revision": "85454b1194e09335b728c20ef378b21a",
+    "url": "/static/media/facebook.85454b11.svg"
+  },
+  {
+    "revision": "4828d900ce1e2a128b000ede42c64b29",
+    "url": "/static/media/github.4828d900.svg"
+  },
+  {
+    "revision": "cadb3733b2476c50ce9147227e3aabc0",
+    "url": "/static/media/mail.cadb3733.svg"
+  },
+  {
+    "revision": "5e0de12f6d4222b444d779885baf9b96",
+    "url": "/static/media/phone.5e0de12f.svg"
+  },
+  {
+    "revision": "0af7620c8217e5556b5e519bbfe10224",
+    "url": "/static/media/profileImg.0af7620c.jpg"
+  }
+]);
