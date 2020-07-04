b0dc650f0cc87915f089936216ef6e86f409d34a
Vachana Chongruchipinyo <bccvachana@gmail.com>
2020-07-05 05:35:41 +0700

    Updates

---
+++ precache-manifest.0bf82e41dc01d75084b3775f4b4ab3ee.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622de62633640538cc47f616d1720974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd768e6bd1c065ee095",
+    "url": "/static/css/main.d1620394.chunk.css"
+  },
+  {
+    "revision": "26676b0a565f41bdd8e9",
+    "url": "/static/js/2.a8de03a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8de03a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd768e6bd1c065ee095",
+    "url": "/static/js/main.e424eead.chunk.js"
+  },
+  {
+    "revision": "9b1312763ecc800e9ffd",
+    "url": "/static/js/runtime-main.4cbfffd7.js"
+  },
+  {
+    "revision": "e62b3130a1faa3434353e3c7f5ff2664",
+    "url": "/static/media/Prompt-Black.e62b3130.ttf"
+  },
+  {
+    "revision": "befa1f27b3fbec48e74d58e8f7d61bb8",
+    "url": "/static/media/Prompt-Bold.befa1f27.ttf"
+  },
+  {
+    "revision": "45f79fcebe7cf35186daed3a2e4e3260",
+    "url": "/static/media/Prompt-ExtraBold.45f79fce.ttf"
+  },
+  {
+    "revision": "cec66179811c4a71396216ef7214f4a0",
+    "url": "/static/media/Prompt-ExtraLight.cec66179.ttf"
+  },
+  {
+    "revision": "2aed5273600290713f7ed8d182a93592",
+    "url": "/static/media/Prompt-Light.2aed5273.ttf"
+  },
+  {
+    "revision": "bd536c6341c5402b41fa4b9e58705579",
+    "url": "/static/media/Prompt-Medium.bd536c63.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/static/media/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "ba9b20ec2ff176605b2d69723af3a54f",
+    "url": "/static/media/Prompt-SemiBold.ba9b20ec.ttf"
+  },
+  {
+    "revision": "ddcfbf895b67206d003167df83c7020a",
+    "url": "/static/media/Prompt-Thin.ddcfbf89.ttf"
+  },
+  {
+    "revision": "85454b1194e09335b728c20ef378b21a",
+    "url": "/static/media/facebook.85454b11.svg"
+  },
+  {
+    "revision": "4828d900ce1e2a128b000ede42c64b29",
+    "url": "/static/media/github.4828d900.svg"
+  },
+  {
+    "revision": "cadb3733b2476c50ce9147227e3aabc0",
+    "url": "/static/media/mail.cadb3733.svg"
+  },
+  {
+    "revision": "5e0de12f6d4222b444d779885baf9b96",
+    "url": "/static/media/phone.5e0de12f.svg"
+  },
+  {
+    "revision": "0af7620c8217e5556b5e519bbfe10224",
+    "url": "/static/media/profileImg.0af7620c.jpg"
+  },
+  {
+    "revision": "990dd06f90af73e1891006a3df970e0a",
+    "url": "/static/media/total1.990dd06f.jpg"
+  },
+  {
+    "revision": "be5da0222140d1e8b2f6383972597646",
+    "url": "/static/media/total2.be5da022.jpg"
+  },
+  {
+    "revision": "4ffda97e3953322101e85a647fd39422",
+    "url": "/static/media/ymc1.4ffda97e.jpg"
+  },
+  {
+    "revision": "c725211679115d36ef850441ebcb40cf",
+    "url": "/static/media/ymc2.c7252116.jpg"
+  },
+  {
+    "revision": "967ee856a9bfdfb45a114aca5c47b4e2",
+    "url": "/static/media/ywc1.967ee856.jpg"
+  },
+  {
+    "revision": "026efaf2bd55f28823bae71e4b51b1cd",
+    "url": "/static/media/ywc2.026efaf2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94146cfaadece9a62bc12eafb75c3486.js"
+  "/precache-manifest.0bf82e41dc01d75084b3775f4b4ab3ee.js"
 );
 
 self.addEventListener('message', (event) => {
